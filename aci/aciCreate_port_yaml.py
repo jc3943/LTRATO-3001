8c8b1b7ed5ed86a386f2122e685034fd2465507b
jc3943 <jcomer@cisco.com>
2022-05-30 06:55:05 -0400

    resync

---
+++ aci/aciCreate_port_yaml.py
@@ -0,0 +1,150 @@
+# Callaghan Donnelly
+# take in a specifically formatted csv file and create a YAML file
+# Section 1: bring in the csv and arrange the data by column in a list of dictionaries
+# Section 2: fill a new dictionary with the reorganized list of dictionaries
+# Section 3: Slap all the new data onto a fresh YAML file
+# Sections are denoted by dash lines (---------)
+
+import yaml
+from yaml import *
+
+import csv
+from csv import *
+
+import xlrd
+from xlrd import *
+
+def main():
+    # initialize all empty needed variables
+    count = 0
+    iter = 0
+    setList = []
+    longlist = []
+    overlist = []
+    bigDict = {}
+    dicttwo = {}
+    dict3 = {}
+
+    # open the CSV file and read it's contents to a list of dictionaries
+    with open('vars/pod1/aci/port-defs.csv', 'r') as csv_file:
+        csvread = csv.DictReader(csv_file)
+        for column in csvread:
+            # append each dict to the list
+            setList.append(column)
+            # make a list of keys (in this case it is all the columns)
+            if count == 0:
+                keyList = findKeys(column)
+            count += 1
+
+        # This creates a list for each column of data
+    #    print(keyList)
+        listgroup = [[] for j in range(len(keyList))]
+
+        # give each key a list of it's underlying values (the value of each row in that column)
+        for i in range(len(keyList)):
+            keyvar = keyList[i]
+            for k in range(count):
+                listgroup[i].append(setList[k][keyvar])
+
+            d = {keyvar: listgroup[i]}
+            dicttwo.update(d)
+
+#     print(dicttwo)
+
+    # -----------------------------------------------------------------
+    # newDict1 is the dictionary that stores all of the info, newDict2 is the dictionary that is used to format the info as it is created
+    newDict1 = {'policyGrps': longlist}
+
+    # find if and where there are repeated entries, so that they won't get repeated in policyGrps
+    for k in range(len(dicttwo['name'])):
+        try:
+            newspot = dicttwo['name'][k+1]
+        except IndexError:
+            newspot = 'null'
+
+        if dicttwo['name'][k] == newspot:
+            overlist.append(newspot)
+
+#        print(overlist)
+
+    # for every interaction that is given in the sheet, run the stuff
+    for q in range(len(dicttwo['interactions'])):
+        # if the interaction we are looking at is policyGrps
+        if dicttwo['interactions'][q] == 'policyGrps':
+            # for every row within the sheet
+            for k in range(len(dicttwo['name'])):
+                # only run if it is not a duplicate of another item
+                if dicttwo['name'][k] not in overlist:
+                    # clear newDict2 every time we run so it can be reused
+                    newDict2 = {}
+                    # add each item with it's corresponding element for this item(row)
+                    newDict2.update({"name": dicttwo['name'][k]})
+                    newDict2.update({"speed": dicttwo['speed'][k]})
+
+                    if dicttwo['intfType'][k] == "vpc":
+                        newDict2.update({"lagType": "node"})
+
+                    elif dicttwo['intfType'][k] == "access":
+                        newDict2.update({"lagType": "leaf"})
+
+                    newDict2.update({"cdpPol": dicttwo['cdpPol'][k]})
+                    newDict2.update({"lldpPol": dicttwo['lldpPol'][k]})
+                    newDict2.update({"mcpPol": dicttwo['mcpPol'][k]})
+                    newDict2.update({"monPol": dicttwo['monPol'][k]})
+                    newDict2.update({"stormCtlPol": dicttwo['stormCtlPol'][k]})
+                    newDict2.update({"lacpPol": dicttwo['lacpPol'][k]})
+                    newDict2.update({"aep": dicttwo['aep'][k]})
+
+                    # add this row to the list so we can clear the dictionary for the next run
+                    longlist.append(newDict2)
+                # if it is a duplicate, then clear it out, and allow only the original to be output
+                if dicttwo['name'][k] in overlist:
+                    rid = dicttwo['name'][k]
+                    overlist.remove(rid)
+
+
+        # if the interaction we are dealing with is interfaceProfiles
+        elif dicttwo['interactions'][q] == 'interfaceProfiles':
+            # create and add a list to the overall dictionary
+            newlist = []
+            setin = {'interfaceProfiles': newlist}
+            newDict1.update(setin)
+            # again, run for every item in the sheet
+            for k in range(len(dicttwo['name'])):
+                newDict2 = {}
+                newDict2.update({"name": dicttwo['name'][k]})
+                newDict2.update({"description": dicttwo['description'][k]})
+                newDict2.update({"interfaceProfile": dicttwo['interfaceProfile'][k]})
+                newDict2.update({"policyGrp": dicttwo['name'][k]})
+                newDict2.update({"blkNum": dicttwo['blkNum'][k]})
+
+                if dicttwo['intfType'][k] == "vpc":
+                    newDict2.update({"intfType": "vpc"})
+
+                elif dicttwo['intfType'][k] == "access":
+                    newDict2.update({"intfType": "switch_port"})
+
+                fromPort = dicttwo['fromPort'][k]
+                toPort = dicttwo['toPort'][k]
+                newDict2.update({"fromPort": int(fromPort)})
+                newDict2.update({"toPort": int(toPort)})
+
+                newlist.append(newDict2)
+
+
+    # -------------------------------------------------------------------------
+
+    # print(newDict1)
+
+    # print all of the key : value pairs to the yaml file
+    with open('vars/pod1/aci/aci-ports.yaml', 'w+') as yml_file:
+        shove = yaml.dump(newDict1, yml_file)
+
+    # print("Success!")
+
+# function to pull all the keys out (for creating lists for all of them and finding their values)
+def findKeys(dict):
+    return [*dict]
+
+
+main()
---
+++ aci/aciCreate_yaml_from_csv_v4.3.py
@@ -0,0 +1,168 @@
+#!/bin python
+# Jeff Comer
+# script to take in a csv seed file and convert to yaml
+# example input csv is vars/<branch>/tenants.csv
+# old, needs re-written using dictionaries for efficiency
+"""
+"""
+
+import csv, sys, getopt
+
+def main(argv):
+    """
+    Main execution routine
+
+    :return: None
+    """
+
+    inputfile = ''
+    outputfile = ''
+    try:
+      opts, args = getopt.getopt(argv,"hi:o:",["ifile=","ofile="])
+    except getopt.GetoptError:
+      print('create_yaml_from_csv_v4.1.py -i <inputfile> -o <outputfile>')
+      sys.exit(2)
+    for opt, arg in opts:
+      if opt == '-h':
+         print('create_yaml_from_csv_v4.1.py -i <inputfile> -o <outputfile>')
+         sys.exit()
+      elif opt in ("-i", "--ifile"):
+         inputfile = arg
+      elif opt in ("-o", "--ofile"):
+         outputfile = arg
+
+    yaml = open(outputfile, "w")
+    brdom = []
+    dfgw = []
+    subnetmask =[]
+    tenant = []
+    vrf = []
+    app_profile = []
+    description = []
+    epg_name = []
+    domain = []
+    domainType = []
+    contract = []
+    encaps = []
+    encaptype = []
+    filter = []
+    L3Out = []
+    multivrf = []
+    rp = []
+    i = 0
+    with open(inputfile) as csv_file:
+       csv_reader = csv.reader(csv_file, delimiter=',')
+       for row in csv_reader:
+         brdom.append(row[0])
+         dfgw.append(row[1])
+         subnetmask.append(row[2])
+         tenant.append(row[3])
+         vrf.append(row[4])
+         app_profile.append(row[5])
+         description.append(row[6])
+         epg_name.append(row[7])
+         domain.append(row[8])
+         domainType.append(row[9])
+         contract.append(row[10])
+         encaps.append(row[11])
+         encaptype.append(row[12])
+         filter.append(row[13])
+         L3Out.append(row[14])
+         multivrf.append(row[15])
+         rp.append(row[16])
+         row_count = i
+         i = i + 1
+       u_tenant_list = []
+       for x in tenant:
+         if x not in u_tenant_list :
+           u_tenant_list.append(x)
+       yaml.write("tenants:\n")
+       for x in u_tenant_list:
+         yaml.write("  - tenant: " + x + "\n")
+       u_vrf_list = []
+       vrftenant = []
+       for x,y in zip(vrf, tenant):
+         if x not in u_vrf_list:
+           u_vrf_list.append(x)
+           vrftenant.append(y)
+       yaml.write("vrfs:\n")
+       for x, y, z in zip(u_vrf_list, vrftenant, rp):
+         yaml.write("  - vrf: " + x + "\n")          
+         yaml.write("    tenant: " + y + "\n")
+         yaml.write("    rp: " + z + "\n")
+       u_ap_list = []
+       aptenant = []
+       for x,y in zip(app_profile, tenant):
+         if x not in u_ap_list:
+           u_ap_list.append(x)
+           aptenant.append(y)
+       yaml.write("aps:\n")
+       for x, y in zip(u_ap_list, aptenant):
+         yaml.write("  - ap: " + x + "\n")
+         yaml.write("    tenant: " + y + "\n")
+       yaml.write("bridge_domains:\n")
+       for v, w, x, y, z, zz in zip(tenant, vrf, brdom, dfgw, subnetmask, L3Out):
+         yaml.write("  - bd: " + x + "\n")
+         yaml.write("    gateway: " + y + "\n")
+         yaml.write("    mask: " + z + "\n")
+         yaml.write("    tenant: " + v + "\n")
+         yaml.write("    vrf: " + w + "\n")
+         yaml.write("    scope: public,shared" + "\n")
+         yaml.write("    L3Out: " + zz + "\n")
+       yaml.write("epgs:\n")
+       for a, b, c, d, u, v, w, x, y, yy, z, zz in zip(dfgw, subnetmask, description, multivrf, tenant, app_profile, epg_name, brdom, domain, domainType, encaps, encaptype):
+         yaml.write("  - epg: " + w + "\n")
+         yaml.write("    tenant: " + u + "\n")
+         yaml.write("    ap: " + v + "\n")
+         yaml.write("    bd: " + x + "\n")
+         yaml.write("    domain: " + y + "\n")
+         yaml.write("    domainType: " + yy + "\n")
+         yaml.write("    encaps: " + z + "\n")
+         yaml.write("    encaptype: " + zz + "\n")
+         yaml.write("    gateway: " + a + "\n")
+         yaml.write("    mask: " + b + "\n")
+         yaml.write("    description: " + c + "\n")
+         yaml.write("    multivrf: " + d + "\n")
+       yaml.write("epg_contracts:\n")
+       for w, x, y, z in zip(tenant, epg_name, contract, app_profile):
+         yaml.write("  - epg: " + x + "\n")
+         yaml.write("    contract: " + y + "\n")
+         yaml.write("    contract_type: \"provider\"\n")
+         yaml.write("    ap: " + z + "\n")
+         yaml.write("    tenant: " + w + "\n")
+         yaml.write("  - epg: " + x + "\n")
+         yaml.write("    contract: " + y + "\n")
+         yaml.write("    contract_type: \"consumer\"\n")
+         yaml.write("    ap: " + z + "\n")
+         yaml.write("    tenant: " + w + "\n")
+       u_contract_list = []
+       contract_tenant = []
+       contract_filter = []
+       for x, y, z in zip(contract, tenant, filter):
+         if x not in u_contract_list:
+           u_contract_list.append(x)
+           contract_tenant.append(y)
+           contract_filter.append(z)
+       yaml.write("contracts:\n")
+       for x, y, z in zip(u_contract_list, contract_tenant, contract_filter):
+         yaml.write("  - contract: " + x + "\n")
+         yaml.write("    tenant: " + y + "\n")
+         yaml.write("    subject: \"Subject\"\n")
+         yaml.write("    filter: " + z + "\n")
+       u_filter_list = []
+       filter_tenant = []
+       for x, y in zip(filter, tenant):
+         if x not in u_filter_list:
+           u_filter_list.append(x)
+           filter_tenant.append(y)
+       yaml.write("filters:\n")
+       for x, y in zip(u_filter_list, filter_tenant):
+         yaml.write("  - filter: " + x + "\n")
+         yaml.write("    tenant: " + y + "\n")
+         yaml.write("    entry: \"default\"\n")
+         yaml.write("    ethertype: \"unspecified\"\n")
+    csv_file.close()
+    yaml.close()
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
