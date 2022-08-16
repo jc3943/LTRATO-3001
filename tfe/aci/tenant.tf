8ac80054afdf4340ecbce807dc06eaa8df6d0c6e
jc3943 <jcomer@cisco.com>
2022-08-16 19:45:54 -0400

    updated aci tfe for tenant

--- tfe/aci/tenant.tf
+++ tfe/aci/tenant.tf
@@ -10,6 +10,7 @@ terraform {
 provider "aci" {
       # cisco-aci user name
       username = "ansible"
+      #password = "DEVP@ssw0rd"
       # private key path
       private_key = "../creds/ansible.key"
       # Certificate Name
@@ -19,9 +20,23 @@ provider "aci" {
       insecure = true
 }
 
-resource "aci_tenant" "example" {
-  name        = "demo_tenant"
+locals {
+  instances = csvdecode(file("./tenant.csv"))
+  #instances = csvdecode(local.csv_data)
+}
+
+resource "aci_tenant" "aci-tenant" {
+  for_each = { for inst in local.instances : inst.bridgeDomain => inst }
+  name        = each.value.tenant
   description = "from terraform"
   annotation  = "tag"
   name_alias  = "tenant"
+}
+
+resource "aci_vrf" "aci-vrf" {
+  for_each = { for inst in local.instances : inst.bridgeDomain => inst }
+  name        = each.value.vrf
+  description = "from terraform"
+  annotation  = "tag"
+  name_alias  = "vrf"
 }
