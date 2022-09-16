ab41720ecfa322764d42f1904ea26e3849cf4cd8
jc3943 <jcomer@cisco.com>
2022-09-16 08:24:34 -0700

    changed node profile filter mechanism

--- tfe/intersight/hxEdgeBuild/hx_node_profile.tf
+++ tfe/intersight/hxEdgeBuild/hx_node_profile.tf
@@ -14,7 +14,7 @@ resource "intersight_hyperflex_node_profile" "hx_node_profile_1" {
 resource "intersight_hyperflex_node_profile" "hx_node_profile_2" {
   name = "${var.cluster_name}-02"
   assigned_server {
-    selector = "$filter=Serial eq 'FCH2109V2RF'"
+    selector = "$filter=Name eq 'sandbox-02'"
     //moid = intersight_hyperflex_cluster_profile.hx_cluster_profile.moid
     object_type = "compute.RackUnit"
   }
@@ -26,7 +26,7 @@ resource "intersight_hyperflex_node_profile" "hx_node_profile_2" {
 resource "intersight_hyperflex_node_profile" "hx_node_profile_3" {
   name = "${var.cluster_name}-03"
   assigned_server {
-    selector = "$filter=Serial eq 'FCH2116V0EN'"
+    selector = "$filter=Name eq 'sandbox-03'"
     //moid = intersight_hyperflex_cluster_profile.hx_cluster_profile.moid
     object_type = "compute.RackUnit"  
   }
